d91f6334202d4d5af941e1b73035c846c92b3dc6
Roger Araujo <araujoroger2402@gmail.com>
2022-12-06 01:19:31 -0400

    agregamos el estilo de la list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import Navbar from "./components/Navbar"
+import TodoList from "./components/todoList";
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      
+      <div className='todo-list'>
+      <TodoList />
+      </div>      
     </div>
   );
 }
---
+++ src/components/todoList.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const TodoList = () =>{
+    return(
+        <div className="list-container">
+            <div className="list-header">
+                <div className="list-items">
+                    <h1>tasks to complete</h1>
+                    <h1>tasks completed</h1>
+                    <h1>tasks deleted</h1>
+                </div>
+
+            </div>
+            <div className="list-body">
+                <h1>hola</h1>
+
+            </div>
+        </div>
+    );
+}
+
+
+export default TodoList
